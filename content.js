246acb92cc6919f11d07a81c667895d32845d009
Nicolas Codonho <35177809+juststudies@users.noreply.github.com>
2020-04-24 15:17:47 -0300

    new speeds, new architecture

--- content.js
+++ content.js
@@ -1,18 +1,38 @@
-const interval = setInterval(()=>{
-    const header = document.querySelector('._3auIg');
-    if(header){
-        clearInterval(interval);
-        const button = document.createElement('button');
-        button.innerHTML = '2x';
-        button.classList.add('twoTimesButton');
-        button.addEventListener('click', ()=>{
-            const audios = document.querySelectorAll('audio');
-            audios.forEach(audio=>{
-                audio.playbackRate = 2;
-            });
-        });
-
-        header.appendChild(button);
+async function Header(){
+    const header = await document.querySelector('._3auIg');
     
-    }
-}, 1000);
+    // if(header){
+        //     clearInterval(interval);
+    // const button = document.createElement('button');
+    // button.innerHTML = '2x';
+    // button.classList.add('twoTimesButton');
+    // button.addEventListener('click', ()=>{
+    //     const audios = document.querySelectorAll('audio');
+    //     audios.forEach(audio=>{
+    //         audio.playbackRate = 2;
+    //     });
+    // });
+    const list = document.createElement('select');
+    list.classList.add('speedChange');
+
+    const speeds = [1, 1.5, 2, 3];
+    var opt_str = '';
+
+    speeds.forEach((speed)=>{
+        opt_str += `<option value="${speed}"> ${speed}x </option>`
+    })
+    list.innerHTML = opt_str;
+
+    list.addEventListener('change', (ev)=>{
+        const audios = document.querySelectorAll('audio');
+        audios.forEach(audio=>{
+            audio.playbackRate = ev.target.value;
+        });
+    })
+
+    header.appendChild(list);
+    
+    // }
+}
+
+Header();
